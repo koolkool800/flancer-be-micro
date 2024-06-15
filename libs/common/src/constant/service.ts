3b2b6067b0e5c8bd3ec130cefb62752663687448
barokool <knabao7a7@gmail.com>
2024-06-16 01:00:38 +0700

    update folder structure

---
+++ libs/common/src/constant/service.ts
@@ -0,0 +1,1 @@
+export const AUTH_SERIVCE = 'AUTH_SERVICE';
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,6 +1,19 @@
+import { configuration } from '@app/common/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: configuration().database.host,
+      port: configuration().database.port,
+      username: configuration().database.username,
+      password: configuration().database.password,
+      database: configuration().database.database,
+      entities: [],
+      synchronize: false,
+    }),
+  ],
 })
 export class DatabaseModule {}
